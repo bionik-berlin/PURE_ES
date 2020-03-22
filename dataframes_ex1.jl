a1cf1ca5e54864a337d7b16695f6220845e540fa
isk <isantibanezk@gmail.com>
2020-03-22 17:20:02 +0100

    Erste stat. analyse

--- dataframes_ex1.jl
+++ dataframes_ex1.jl
@@ -1,4 +1,41 @@
 using DataFrames
 using CSV
-df = DataFrame(A = 1:4, B = ["M", "F", "F", "M"])
-CSV.write("test1.csv", df)
+using Random
+function q(x)
+    sum(x.^2)
+end
+
+function es_msr(x₀,λ,σ₀,qₜ,N)
+    xₑ=x₀
+    σ=σ₀
+    γ=0
+    qₑ=q(xₑ)
+    while (qₑ>qₜ)
+        σₙ=[σ*exp(rand([-1.0,1.0])) for l=1:λ]
+        xₙ=[xₑ+σₙ[l]*randn(N) for l=1:λ]
+        qₙ=q.(xₙ)
+        idx=sortperm(qₙ)
+        xₑ=xₙ[idx[1]]
+        σ=σₙ[idx[1]]
+        qₑ=qₙ[idx[1]]
+        γ=γ+1
+    end
+    (γ,xₑ,qₑ,σ)
+end
+
+REP=150
+N=100
+G1=[]
+G2=[]
+for r=1:REP
+    res1=es_msr(ones(N),10,1/sqrt(N),1.0e-10,N)
+    res2=es_msr(ones(N),27,1/sqrt(N),1.0e-10,N)
+#    @show[res[1]]
+    push!(G1,res1[1])
+    push!(G2,res2[1])
+end
+df = DataFrame(id = 1:length(G), x1 = G1, x2=G2)
+if (pwd()=="/home/iskl")
+    cd("github/PURE_ES")
+end
+CSV.write("./test1.csv", df)
